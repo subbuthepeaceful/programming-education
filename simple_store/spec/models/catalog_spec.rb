62a8e5ea348ab22c31c5e4bc2f19b14bf5d03f34
Subbu Balakrishnan <subbuthepeaceful@gmail.com>
2015-02-05 17:37:38 -0800

    Added catalog spec

--- simple_store/spec/models/catalog_spec.rb
+++ simple_store/spec/models/catalog_spec.rb
@@ -1,11 +1,9 @@
-describe Catalog do
-  it 'should have a Name' do
-    c = Catalog.create(name: 'Master Simple Store Catalog')
-  end
+require 'rails_helper'
 
+describe Catalog do
   it 'should have at least one Catalog that is active' do
-    c = Catalog.where(active: true)
-    c.should_not be_empty
+    active_catalogs = Catalog.where(active: true)
+    expect(active_catalogs).not_to be_empty
   end
 
 end
