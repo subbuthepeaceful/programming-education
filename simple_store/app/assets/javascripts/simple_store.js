e84756eafb37ad3b8eadf1799f88fe6f5411cac3
Subbu Balakrishnan <subbuthepeaceful@gmail.com>
2015-02-12 18:03:24 -0800

    Added some javascript for products

---
+++ simple_store/app/assets/javascripts/simple_store.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $("a#featured-apparel-product").click(function() {
+    var productId = $(this).attr("data_product_id");
+    var productUrl = "/products/" + productId + ".json";
+    $.get(productUrl, function(response) {
+      $("div.luggage").hide();
+      $("div.cutlery").hide();
+      var featuredHTML = response.name + "<br />" + response.description;
+      $("div.apparel-featured").append(featuredHTML);
+      $("div.apparel-featured").show();
+    });
+  });
+});
--- simple_store/config/routes.rb
+++ simple_store/config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
     resources :categories
     resources :products
   end
+
+  resources :products
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
