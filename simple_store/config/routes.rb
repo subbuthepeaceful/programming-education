1669ce85ba4593fc092f816cb09ebc441a987ae7
Subbu Balakrishnan <subbuthepeaceful@gmail.com>
2015-02-13 05:23:33 -0800

    Added a simple namespaced API for products

--- simple_store/config/routes.rb
+++ simple_store/config/routes.rb
@@ -5,7 +5,11 @@ Rails.application.routes.draw do
     resources :products
   end
 
-  resources :products
+  namespace :api do 
+    namespace :v1 do 
+      resources :products, only: [:index, :show]
+    end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
