ac2c043b6ceff6387ec9fd4e46a4aca7b1a32f63
Subbu Balakrishnan <subbuthepeaceful@gmail.com>
2015-02-18 16:22:54 -0800

    Updated to include topical in User and turn off spring for development

--- project_tracking/app/models/user.rb
+++ project_tracking/app/models/user.rb
@@ -14,4 +14,16 @@ class User < ActiveRecord::Base
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def matching_projects
+    matched_projects = []
+    topics.each do |topic|
+      Project.all.each do |project|
+        if project.is_related_to?(topic.title)
+          matched_projects << {project: project, topic: topic}
+        end
+      end
+    end
+    matched_projects
+  end
 end
--- project_tracking/app/utilities/topical.rb
+++ project_tracking/app/utilities/topical.rb
@@ -3,5 +3,13 @@ module Utilities
     def display_number_of_topics
       puts "#{topics.size} Topics"
     end
+
+    def is_related_to?(topic_name)
+      topics.map { |t| t.title }.include?(topic_name)
+    end
+
+    def pretty_print_topics
+      topics.map { |t| t.title }.join(", ")
+    end
   end
 end
