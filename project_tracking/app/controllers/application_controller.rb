79bb8525869df629a68bffe3157ad00856d09f73
Subbu Balakrishnan <subbuthepeaceful@gmail.com>
2015-02-24 08:51:45 -0800

    Added multi-locale elements to support Session 21 - Internationalization

--- project_tracking/app/controllers/application_controller.rb
+++ project_tracking/app/controllers/application_controller.rb
@@ -3,10 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :set_locale
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation) }
+  end
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 
 end
